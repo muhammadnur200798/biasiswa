197606d142cf392ae169419c0760402569669fcc
muhammadnur200798 <67585700+muhammadnur200798@users.noreply.github.com>
2020-06-29 22:08:18 +0800

    Add files via upload

---
+++ home.php
@@ -0,0 +1,10 @@
+<body bgcolor="Pink">
+<h1><p align="center">
+<font color="maroon">
+   	Selamat Datang di Sistem Pendukung Keputusan <br>
+	Penentuan Penerima Beasiswa di STMIK AKAKOM Yogyakarta <br>
+	Menggunakan Metode Simple Additive Weighting (SAW)
+</h1> 
+</b>
+</p>
+</font>
